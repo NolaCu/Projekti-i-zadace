8fce032420ad884d75c764a5655f1dece4815751
Nola Čumlievski <58535424+NolaCu@users.noreply.github.com>
2020-11-24 20:55:36 +0100

    Update and rename neuralnemreze.R to Vjezba - neuralne mreže u R-u.R

--- Vjezba - neuralne mreže u R-u.R
+++ Vjezba - neuralne mreže u R-u.R
@@ -9,34 +9,43 @@ View(y)
 barplot(y)
 barplot(y[order(y)]) #uređen plot
 barplot(y[order(y)], horiz = TRUE) #horizontalni barplot
+
 #različite boje
 barplot(y[order(y)], horiz = TRUE, col=c("orange", "blue", "red", "green"))
+
 #micanje okvira stupaca
 barplot(y[order(y)], horiz = TRUE, col=c("orange", "blue", "red", "green"), 
         border=NA, main = "Barplot \n frekvencija regija",
         xlab = "Frekvencije", ylab = "Regije")
+
 #histogram
 hist(korisnici$Age)
+
 #grupirati stupce
 hist(korisnici$Age, breaks = 5)
+
 #dodati dužine kategorija
 hist(korisnici$Age, breaks = c(0,40,60,100), freq=TRUE, col=c("yellow", "orange", "red"),
      main="Histogram frekvencije prema godinama", xlab = "Godine", ylab = "Frekvencija")
+
 library(keras)
 fashion = dataset_fashion_mnist()
+
 #pridruzivanje x i y varijablama
 c(train_slike, train_oznake) %<-% fashion$train 
 c(test_slike, test_oznake) %<-% fashion$test
 dim(train_slike)
 str(train_slike)
+
 #vizualizacija slike
 objekt = train_slike[5,,]#peta slika
 plot(as.raster(objekt, max=255)) #pikselizirana slika
 
 #klase
-imena = c("majica/top", "gaće", "pulover", "haljina", "kaput", "sandale", "majica", "patike", "torba", "čizme")
+imena = c("majica/top", "gaÄ‡e", "pulover", "haljina", "kaput", "sandale", "majica", "patike", "torba", "Äizme")
 imena[train_oznake[5]+1]#ide +1 jer oznake počinju s 0
-#normalizacija podataka - (x-mean)/std - ova formula se koristi kada psotoje heterogeni pdoaci
+
+#normalizacija podataka - (x-mean)/std - ova formula se koristi kada psotoje heterogeni podaci
 train_slike = train_slike/255
 test_slike = test_slike/255 #dilimo sa 255 kako bi sve vrijednosti bile između 0 i 1
 
@@ -46,7 +55,8 @@ val_slike = train_slike[indeks,,]
 part_train_slike = train_slike[-indeks,,]
 val_oznake = train_oznake[indeks]
 part_train_oznake = train_oznake[-indeks]
-#sekvencijalni model se radi prilikon izrade kompleksnih mreža (koje se sastoje od više istih), sekvencijalni za običnu 
+
+#sekvencijalni model se radi prilikon izrade kompleksnih mreža (koje se sastoje od viÅ¡e istih), sekvencijalni za običnu 
 model = keras_model_sequential()
 
 #struktura modela
@@ -60,8 +70,8 @@ model %>% compile(optimizer = "sgd",
                   loss = "sparse_categorical_crossentropy", 
                   metrics = c("accuracy")) # zadnji nije obavezan ali se koristi za pregled performanse modela
 #binary crossentropy - ako objekt pripada jednoj od ukupno dvi klase
-#sparse categorical crossentropy - koristi se kada imamo više od 2 klase i objekt pripada isključivo jednoj od tih
-#categorical crossentropy - kad imamo više klasa i objekt može pripasti više od jedne
+#sparse categorical crossentropy - koristi se kada imamo viÅ¡e od 2 klase i objekt pripada iskljuÄivo jednoj od tih
+#categorical crossentropy - kad imamo viÅ¡e klasa i objekt moÅ¾e pripasti viÅ¡e od jedne
 
 #kreiranje modela
 model %>% fit(part_train_slike, part_train_oznake, epochs = 30, batch_size = 100, #broj opservacija kod svake forawrd i backward propagacije,
@@ -74,7 +84,7 @@ score = model %>% evaluate(test_slike, test_oznake)
 #predikcija
 predikcija = model %>% predict(test_slike)
 predikcija[1,]
-which.max(predikcija[1,])#najveća vjerojatnost klase
+which.max(predikcija[1,])#najveÄ‡a vjerojatnost klase
 imena[which.max(predikcija[1,])]
 plot(as.raster(test_slike[1,,], max=1))
 
@@ -96,7 +106,8 @@ df = data.frame(sati, test, prolaz)
 nn = neuralnet(prolaz~sati+test, data = df, hidden = c(3,2), 
                act.fct = "logistic", linear.output = FALSE) #false za klasifikaciju
 plot(nn)
-#crne su te�ine, plave biasi
+
+#crne su težine, plave biasi
 tsati = c(20, 20, 30)
 ttest = c(80, 30, 20)
 testdf = data.frame(tsati, ttest)
@@ -120,7 +131,7 @@ std_train = attr(train_data, "scaled:scale")
 test_data = scale(test_data, center = mean_train, scale = std_train)
 
 #definiranje inputa
-input = layer_input(shape = dim(train_data)[2])#2 zato šta to predstavlja broj varijabli
+input = layer_input(shape = dim(train_data)[2])#2 zato Å¡ta to predstavlja broj varijabli
 #output sloj
 predikcije = input %>%
   layer_dense(units = 64, activation = "relu") %>%
@@ -158,10 +169,10 @@ save_model_hdf5(model_f, "model.h5")
 #callbackovi na rezutlate pojedine epohe
 checkpoint_dir = "checkpoints"
 dir.create(checkpoint_dir, showWarnings = FALSE)
-filepath = file.path(checkpoint_dir, "Epoch-{epoch:02d}.hdf5")#svaka epoha zvat će se epoch - 01,02,...
+filepath = file.path(checkpoint_dir, "Epoch-{epoch:02d}.hdf5")#svaka epoha zvat Ä‡e se epoch - 01,02,...
 cp_callback = callback_model_checkpoint(filepath = filepath)
 rm(model_f)
-k_clear_session() #cisti memoriju, počinje bez označenih težina
+k_clear_session() #cisti memoriju, poÄinje bez oznaÄenih teÅ¾ina
 model_cb = keras_model(inputs = input_f, outputs = main_output)
 model_cb %>% compile(optimizer = "rmsprop", loss="mse", metrics = list("mean_absolute_error"))
 model_cb %>% fit(train_data, train_oznake, epochs = 30, callbacks = list(cp_callback))
@@ -180,7 +191,7 @@ model_cb_naj %>% fit(train_data, train_oznake, epochs = 30, validation_data = li
                      callbacks = list(cp_callback_naj))
 naj_model = load_model_hdf5("najbolji.h5")
 
-#stopiranje kad dođemo do najboljeg modela
+#stopiranje kad doÄ‘emo do najboljeg modela
 callacks_list = list(callback_early_stopping(monitor = "val_loss", patience = 3), #koliko epoha gledamo jel ima napretka
                      callback_model_checkpoint(filepath = "najbolji_stopirani.h5", monitor = "val_loss", save_best_only = TRUE))
 rm(model_cb_naj)
@@ -196,11 +207,13 @@ k_clear_session()
 df = read.csv("/home/nola/Documents/Files/Files for add-on section/Data/House_Price.csv", header = TRUE)
 str(df)
 summary(df) #univarijantna analiza podataka
-#gledamo razliku između kvartala za svaku varijablu kako bi identificirali outliere i skewness
-#kako bi saznali šta nije u redu tribamo plotat varijable
+
+#gledamo razliku izmeÄ‘u kvartala za svaku varijablu kako bi identificirali outliere i skewness
+#kako bi saznali Å¡ta nije u redu tribamo plotat varijable
 hist(df$crime_rate)
 pairs(~df$price+df$crime+df$n_hot_rooms+df$rainfall)
-#barplotovi kategoričkih varijabli
+
+#barplotovi kategoriÄkih varijabli
 barplot(table(df$airport))
 barplot(table(df$waterbody))
 barplot(table(df$bus_ter))
@@ -235,9 +248,9 @@ df$avg_dist = (dist1 + dist2 + dist3 + dist4)/4
 df = df[,c(-7,-8,-9,-10)] #ili -7:-10
 df = df[,-14]
 
-#dummy varijable - na inkorporaciju kategoričkih varijabli u regresiju
+#dummy varijable - za inkorporaciju kategoričkih varijabli u regresiju
 #to je ono poseban stupac za svaku kategoriju
-#ne postoji hijerarhija u ovin varijablama, ne mogu se kodirati brojevima jer se oni mogu poredati po veličini
+#ne postoji hijerarhija u ovim varijablama, ne mogu se kodirati u brojevima jer se oni mogu poredati po veličini
 #airport i waterbody convertiramo u dummy varijable
 library(dummies)
 df = dummy.data.frame(df)
@@ -246,7 +259,8 @@ df = df[,c(-9,-15)] #ove micemo jer su redundantne - npr kod airporta se vidi po
 #korelacijska matrica
 cor(df)
 round(cor(df),2)
-#ako nađemo neovisne vrijable koje su visoko kolerirane, tribamo jednu izbacit da izbjegnemo multicolinearnost
+
+#ako nađemo neovisne vrijable koje su visoko kolerirane, tribamo jednu izbacit da izbjegnemo multikolinearnost
 #izbacujemo onu koja ima veću korelaciju sa ovisnon varijablon
 df = df[, -16]
 
